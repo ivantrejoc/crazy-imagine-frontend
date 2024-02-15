3324af5c63bb2750c117eec7733a6391bfe83449
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-02-15 12:28:04 -0400

    first commit

---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+import { LoginForm } from "../components/LoginForm";
+
+const Login = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-medium justify-center text-center mx-auto mt-12 mb-0">Ingresa a Youtube</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default Login;
