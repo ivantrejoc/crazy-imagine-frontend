37bc279250ac94553e8ae9e44c81ee403a921904
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-02-17 08:48:23 -0400

    5 most popular videos done

--- src/app/api/services/api.services.js
+++ src/app/api/services/api.services.js
@@ -5,6 +5,13 @@ export const getVideos = async () => {
     return data;
   };
 
+  export const getPopularVideos = async () => {
+    const URL = "http://localhost:3001/ivan-trejo-challenge/video/popular";
+    const apiVideos = await fetch(URL);
+    const data = await apiVideos.json();
+    return data;
+  };
+
   export const getVideoById = async (id) => {
     const URL = `http://localhost:3001/ivan-trejo-challenge/video/${id}`;
     const apiVideo = await fetch(URL, { next: { revalidate: 30 } });
--- src/app/popular/page.js
+++ src/app/popular/page.js
@@ -1,6 +1,48 @@
-const Popular = () => {
+"use client";
+import { VideoCard } from "../components/VideoCard";
+import { getPopularVideos } from "../api/services/api.services";
+import { useState, useEffect } from "react";
+
+
+
+
+const Popular =  () => {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    async function fetchPopular() {
+      const videosData = await getPopularVideos();
+      setVideos(videosData);
+      setLoading(false);
+    }
+    fetchPopular();
+  }, []);
+
+
+
+
   return (
-    <div>Popular</div>
-  )
-}
-export default Popular
+    <main className="flex flex-col items-center h-fit">
+      <h2 className="mt-2 text-xl font-medium">Videos más populares</h2>
+      {loading ? (
+        <p>Cargando Videos...</p>
+      ) : (
+        <section className="grid lg:grid-cols-3 lg:grid-rows-3 lg:gap-6 md:grid-cols-2 my-6 p-6 md:grid-rows-4 md:gap-2 place-items-center">
+          {videos.map((video) => (
+            <VideoCard
+              key={video.id}
+              id={video.id}
+              title={video.title}
+              link={video.link}
+              likes={video.like_count}
+              unlikes={video.unlike_count}
+            />
+          ))}
+        </section>
+      )}
+    </main>
+  );
+};
+export default Popular;
