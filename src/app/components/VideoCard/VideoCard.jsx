9092be9838d075ae6245d7200d52d4bbf1df5c49
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-02-17 07:10:59 -0400

    like/unlike styles buttons video card

--- src/app/components/VideoCard/VideoCard.jsx
+++ src/app/components/VideoCard/VideoCard.jsx
@@ -3,8 +3,6 @@ import { GrLike } from "react-icons/gr";
 import { GrDislike } from "react-icons/gr";
 
 const VideoCard = ({ id, title, link, likes, unlikes }) => {
-
-
   return (
     <div className=" w-96 flex justify-center items-center">
       <div className="max-w-lg container bg-white rounded-xl shadow-lg transition duration-200 hover:scale-105 hover:shadow-2xl">
@@ -25,18 +23,24 @@ const VideoCard = ({ id, title, link, likes, unlikes }) => {
         </div>
 
         <div className="flex p-4 justify-between">
-          <div className="flex space-x-2 gap-3">
+          <div className=" bg-blue-300 flex space-x-2 gap-3">
             <div className="flex space-x-1 items-center">
               <LiaCommentSolid className="w-6 h-6" />
               <span>no hay</span>
             </div>
             <div className="flex space-x-1 items-center">
-              <GrLike className="w-5 h-5" />
-              <span>{likes}</span>
+              <button className="w-6 h-6 hover:transform p-1 border border-black rounded-full transition duration-200 hover:scale-105 hover:bg-red-500">
+                <GrLike className="w-full h-full" />
+              </button>
+
+              <span className="text-lg font-medium">{likes}</span>
             </div>
             <div className="flex space-x-1 items-center">
-              <GrDislike className="w-5 h-5" />
-              <span>{unlikes}</span>
+              <button className="w-6 h-6 hover:transform p-1 border border-black rounded-full transition duration-200 hover:scale-105 hover:bg-red-500">
+                <GrDislike className="w-full h-full" />
+              </button>
+
+              <span className="text-lg font-medium">{unlikes}</span>
             </div>
           </div>
         </div>
