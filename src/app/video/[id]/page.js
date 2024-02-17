8c05b31284bcbaa89e58534bddcc11422390346e
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2024-02-16 22:55:10 -0400

    revalidate api  30 seg, actualiza likes, unlikes y comments más rápido

--- src/app/video/[id]/page.js
+++ src/app/video/[id]/page.js
@@ -1,12 +1,16 @@
 import { VideoDetail } from "@/app/components/VideoDetail";
 
+
+
 const getVideoById = async (id) => {
   const URL = `http://localhost:3001/ivan-trejo-challenge/video/${id}`;
-  const apiVideo = await fetch(URL);
+  const apiVideo = await fetch(URL, { next: { revalidate: 30 } });
   const data = await apiVideo.json();
 
   return data;
 };
+
+
 
 const VideoById = async (id) => {
   
